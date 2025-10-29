e3eb86e47a2cd058c50df09499d974b4021d47b8
lluis <lluisunyol@gmail.com>
2025-10-29 17:07:24 +0100

    utils for pgRouting

--- data/sql/pgrouting_calculate-route.sql
+++ data/sql/pgrouting_calculate-route.sql
@@ -1,5 +1,7 @@
+-- Obtenemos latitud y longitud de origen y destino
 SELECT * FROM app.sodd_locations WHERE location_id IN ('08019', '08230');
 
+-- Obtenemos puntos de carretera más cercanos de latitud y longitud de origen y destino
 SELECT 'origen' AS tipo, id FROM (
     SELECT id FROM app.ways_vertices_pgr 
     ORDER BY the_geom <-> ST_SetSRID(ST_MakePoint(2.150980391048032, 41.39933854017455), 4326) 
@@ -12,6 +14,7 @@ SELECT 'destino' AS tipo, id FROM (
     LIMIT 1
 ) AS punto_b;
 
+-- Usamos los identificadores para calcular la ruta por carretera:
 SELECT 
     r.seq,
     r.node,
@@ -28,8 +31,8 @@ SELECT
     w.the_geom
 FROM pgr_dijkstra(
     'SELECT gid as id, source, target, cost, reverse_cost FROM app.ways',
-    42976,
-	92986,
+    42976, -- origen
+	92986, -- destino
     directed := true
 ) r
 LEFT JOIN app.ways w ON r.edge = w.gid
